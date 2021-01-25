31e7b93b15596761b38526a894445bd9eb1a87e8
Seia-Soto <seia@outlook.kr>
2021-01-25 18:43:21 +0900

    Add files and initialize new project

---
+++ README.md
@@ -0,0 +1,44 @@
+# Seia-Soto/medium-limit-bypass
+
+A simple chrome extension to bypass medium free reading limit.
+Also, you can get how to bypass permanently without using extension.
+
+> The concept of this extension is proof, not working one.
+
+## Table of Contents
+
+- [Behind the scense (do without extension)](#behind-the-scense)
+- [Usage (do with extension)](#usage)
+
+----
+
+# Behind the scense
+
+Basically, you can do same as this extension does because this is a simple trick.
+Just follow the steps below:
+
+1. Open [`medium.com`](https://medium.com).
+2. Click a lock icon or site information icon before URL begins at search input.
+3. Open or show a window related to Cookie.
+4. Delete or prevent all cookies from `medium.com` or Medium related ones.
+5. Refresh the webpage.
+
+Cookies can track you with unique ID generated because your ID is unique.
+Also, there is no reason that Medium not to use this simple trick.
+
+# Usage
+
+To use this extension, you can with few steps.
+
+1. Clone or download this repository.
+2. Go to 'chrome://extensions' page or your webbrowser extension manager page.
+3. Enable developer mode.
+  - On Chrome, you can toggle the mode with switch at top right.
+4. Load extension from sources or files.
+5. Select `src` folder in this repository.
+6. Refresh the webpage.
+
+# LICENSE
+
+This project is distributed under [MIT license](./LICENSE).
+All icons are from [UXFree.com](https://img.uxfree.com/wp-content/uploads/2017/03/medium-icon-white-on-black.png) and resized.
---
+++ src/background.js
@@ -0,0 +1,106 @@
+// NOTE: Additional term for possible chrome-like browsers;
+browser = chrome || browser
+browser.cookies = browser.cookies || browser.experimental.cookies
+
+const medium = 'medium.com'
+const domainPattern = /^(?:https?:\/\/)?(?:[^@\/\n]+@)?([^:\/?\n]+)/igm
+
+// NOTE: Cache for medium based websites and this should be empty as browser start to prevent false positive cases;
+const mediumLike = []
+
+const buildCookieURL = cookie => {
+  const protocol = cookie.secure ? 'https:' : 'http:'
+
+  return protocol + '//' + cookie.domain + cookie.path
+}
+const buildCookieDetails = cookie => {
+  return {
+    name: cookie.name,
+    storeId: cookie.storeId,
+    url: buildCookieURL(cookie)
+  }
+}
+const getAllCookies = domain => {
+  return new Promise((resolve, reject) => {
+    browser.cookies.getAll({ domain }, cookies => resolve(cookies))
+  })
+}
+const removeCookie = cookie => {
+  return new Promise((resolve, reject) => {
+    browser.cookies.remove(buildCookieDetails(cookie), result => {
+      if (!result) {
+        console.error('Failed to remove cookie:', cookie.domain, cookie.path, cookie.name)
+      } else {
+        console.log('Successfully removed cookie:', result)
+      }
+
+      resolve()
+    })
+  })
+}
+const removeAllCookies = domain => {
+  Promise.all([
+    getAllCookies(domain),
+    getAllCookies('.' + domain)
+  ])
+    .then(sites => {
+      let sitesLn = sites.length
+
+      while (--sitesLn) {
+        const cookies = sites[sitesLn]
+
+        if (!cookies) return
+
+        let cookiesLn = cookies.length
+
+        while (--cookiesLn) {
+          removeCookie(cookies[cookiesLn])
+        }
+      }
+    })
+}
+
+// NOTE: Remove cookies on medium-related cookies added;
+browser.cookies.onChanged.addListener(changedInfo => {
+  const { cookie, removed } = changedInfo
+
+  if (!removed) return
+
+  const isMediumCookie =
+    (cookie.domain.endsWith(medium))
+  const isMediumBasedCookie =
+    (mediumLike.indexOf(cookie.domain) > -1)
+  if (!isMediumCookie || !isMediumBasedCookie) return
+
+  removeCookie(cookie)
+})
+
+// NOTE: Detect cookies on medium-based sites;
+browser.webRequest.onCompleted.addListener(
+  details => {
+    if (!details.url.includes('cdn-client.medium.com') || !details.initiator) return
+
+    const result = domainPattern.exec(details.initiator)
+
+    if (!result) return
+
+    const [, domain] = result
+
+    if (domain && mediumLike.indexOf(domain) < 0) {
+      mediumLike.push(domain)
+      mediumLike.push('.' + domain)
+
+      removeAllCookies(domain)
+
+      console.log('Detected new Medium based website:', domain)
+    }
+  },
+  {
+    urls: [
+      '<all_urls>'
+    ]
+  }
+)
+
+// NOTE: Remove all cookies on initial installation;
+removeAllCookies(medium)
